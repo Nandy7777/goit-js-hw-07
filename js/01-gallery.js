a84eaf6d8250625cc27cafc7cd0b7954646ff123
Nandy7777 <nandy7777@ukr.net>
2022-08-12 19:56:45 +0300

    something 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryContainer = document.querySelector(`.gallery`); 
+const galleryImages = createGallleryItems(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryImages);
+
+function createGallleryItems(galleryItems) {  
+  return galleryItems
+    .map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                    />
+                </a>
+                </div>`;
+        })
+      .join(``);
+    
+    
+};
+
+galleryContainer.addEventListener(`click`, onGalleryContainerClick);
+function onGalleryContainerClick(evt) {
+    evt.preventDefault();
+    console.log(evt.target.dataset.source);
+
+    
+}
+
+const instance = basicLightbox.create(`
+    <img src="${original}" width="800" height="600">
+    `);
+
+instance.show();
+
 console.log(galleryItems);
