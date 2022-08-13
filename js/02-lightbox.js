96adf69f405087545dfc2d186dbcf6e3673b7fc9
Nandy7777 <nandy7777@ukr.net>
2022-08-13 22:52:16 +0300

    2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -8,11 +8,11 @@ galleryContainer.insertAdjacentHTML('beforeend', galleryImages);
 function createGallleryItems(galleryItems) {  
   return galleryItems
     .map(({ preview, original, description }) => {
-        return `<a class="gallery__item" href="${original}">
+        return `<li><a class="gallery__item" href="${original}">
                 <img class="gallery__image"
                 src="${preview}" 
                 alt="${description}" />
-                </a>`;
+                </a></li>`;
         })
       .join(``);
 };
